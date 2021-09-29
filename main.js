5e96b18a610d98db06fcb5f382fa179714b19bc6
stet345434543454345 <83256244+stet345434543454345@users.noreply.github.com>
2021-09-29 15:36:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/v8QMwMnQo/model.json", modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
